4798f5605b22ebe798deb230bb63e0175c68f3ed
Artem Sobakar <sobakar.artyom@gmail.com>
2024-04-20 19:28:58 +0300

    first task done

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,39 @@
-import {encoded, translations} from './data.js'
+// import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// console.log("Let's rock")
+// console.log(encoded, translations)
 
 
 
 // console.log(decoded)
+
+import { encoded, translations } from './data.js';
+
+const decodeFields = (data) => {
+    return data.map(item => {
+        const decodedItem = { groupId: item.groupId };
+        for (const key in item) {
+            if (key.endsWith('Id') && key !== 'groupId') {
+                const id = item[key];
+                const decodedValue = translations[id] || id;
+                decodedItem[key] = decodedValue;
+            } else {
+                decodedItem[key] = item[key];
+            }
+        }
+        return decodedItem;
+    });
+};
+
+const uniqueIds = new Set();
+encoded.forEach(item => {
+    for (const key in item) {
+        if (key.endsWith('Id')) {
+            uniqueIds.add(item[key]);
+        }
+    }
+});
+
+const decoded = decodeFields(encoded);
+console.log(decoded);
+console.log(Array.from(uniqueIds));
